01a4e9e8b09aff5318f51a07248d327dd2001c98
gokite227 <102020249+gokite227@users.noreply.github.com>
2022-07-06 14:47:03 +0900

    Create 220706_2.py

---
+++ 220706_2.py
@@ -0,0 +1,16 @@
+#함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 
+# 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+
+def solution(x, n):
+    answer = []
+    y = x
+    for i in range(0,n):
+        answer.append(y)
+        y = y + x
+    return answer
+
+#####
+def number_generator(x, n):
+    # 함수를 완성하세요
+    return [i * x + x for i in range(n)]
+print(number_generator(2, 5))
